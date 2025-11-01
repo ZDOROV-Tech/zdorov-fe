ab1cb66dde47c5fda35acd683ea3090b2e2e475c
dzherro <mark.bulavin2005@gmail.com>
2025-11-01 19:41:43 +0200

    feat(setup): Installed and configured PrimeNg Library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,9 +4,12 @@ import {
   provideZonelessChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -14,5 +17,7 @@ export const appConfig: ApplicationConfig = {
     provideZonelessChangeDetection(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+    providePrimeNG(),
   ],
 };
